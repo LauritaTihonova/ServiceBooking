e4cbfdb4af03b90c2285bbc83251d49db8b2ec3a
Laurita Tihonova <tihonova.laurita@gmail.com>
2023-04-04 16:44:09 +0300

    HomePage.html

--- src/main/java/com/project/ServiceBooking/controllers/TestController.java
+++ src/main/java/com/project/ServiceBooking/controllers/TestController.java
@@ -1,4 +1,15 @@
 package com.project.ServiceBooking.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class TestController {
+    @RequestMapping(path = "/home")
+    public String homepage() {
+        return "HomePage.html";
+    }
+
 }
+
