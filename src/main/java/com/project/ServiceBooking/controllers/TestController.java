d7f640f9e761772c4803e09db0efe75eceb819af
Kristiana Usacka <kristianausacka@gmail.com>
2023-04-05 16:15:59 +0300

    Specialist profile ui

--- src/main/java/com/project/ServiceBooking/controllers/TestController.java
+++ src/main/java/com/project/ServiceBooking/controllers/TestController.java
@@ -1,20 +1,36 @@
 package com.project.ServiceBooking.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class TestController {
-    @RequestMapping(path = "/home")
-    public String homepage() {
-        return "HomePage.html";
-    }
+        @RequestMapping(path = "/home")
+        public String homepage() {
+            return "HomePage.html";
+        }
 
-    @RequestMapping(path = "/profile")
-    public String userProfile() {
-        return "User_Profile.html";
-    }
+        @RequestMapping(path = "/profile")
+        public String userProfile() {
+            return "User_Profile.html";
+        }
+
+        @GetMapping("/private")
+        public String specialistPrivateProfile(){
+            return "Specialist_Profile_Private.html";
+        }
+        @GetMapping("/private/edit")
+        public String specialistPrivateEdit(){
+            return "Specialist_Profile_Edit.html";
+        }
+
+        @GetMapping("/clientPrivate")
+        public String clientPrivateProfile(){
+            return "Client_Profile_Private.html";
+        }
+
 
 }
 
