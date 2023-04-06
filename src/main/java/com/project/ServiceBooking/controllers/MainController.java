4e0410c43b0c7addd0cb4288fcf9edca50377448
Kristiana Usacka <kristianausacka@gmail.com>
2023-04-06 15:33:30 +0300

    begining to get edit data from ui

--- src/main/java/com/project/ServiceBooking/controllers/MainController.java
+++ src/main/java/com/project/ServiceBooking/controllers/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -39,8 +41,8 @@ public class MainController {
         model.addAttribute("userProfile", user);
         return "Specialist_Profile_Private.html";
     }
-    @GetMapping("/private/edit")
-    public String specialistPrivateEdit(Model model){
+    @PostMapping("/private/edit")
+    public String specialistPrivateEdit(@ModelAttribute User userEdit, Model model){
         User user = clientProfilePrivateService.getUser();
         model.addAttribute("userProfile", user);
         return "Specialist_Profile_Edit.html";
