a1e20cb8b8062a70fe678b2118c79c17abf1ddba
Laurita Tihonova <tihonova.laurita@gmail.com>
2023-04-06 15:08:50 +0300

    small changes in html files

--- src/main/java/com/project/ServiceBooking/controllers/MainController.java
+++ src/main/java/com/project/ServiceBooking/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.project.ServiceBooking.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @Controller
@@ -52,8 +54,6 @@ public class MainController {
     public String register() {
         return "Registration_Two_Options.html";
     }
-
-
 
 
 }
--- src/main/java/com/project/ServiceBooking/repositories/ServiceRepository.java
+++ src/main/java/com/project/ServiceBooking/repositories/ServiceRepository.java
@@ -1,8 +1,10 @@
 package com.project.ServiceBooking.repositories;
 
-import com.project.ServiceBooking.data.Account;
 import com.project.ServiceBooking.data.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
+
 }
+
