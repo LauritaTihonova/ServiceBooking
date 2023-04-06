3003c76c99e49a97265bc6c77275a477abea57c3
Ethorcharl <zenja_riga5@yahoo.com>
2023-04-06 15:00:42 +0300

    Spring Security

--- src/main/java/com/project/ServiceBooking/repositories/UserRepository.java
+++ src/main/java/com/project/ServiceBooking/repositories/UserRepository.java
@@ -1,8 +1,16 @@
 package com.project.ServiceBooking.repositories;
 
-import com.project.ServiceBooking.data.Account;
+
 import com.project.ServiceBooking.data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+    @Query(value = "select * from users u where u.email like %:enterEmail% ", nativeQuery = true)//without nativeQuery doesn't  work
+    User findByEnterEmail(@Param("enterEmail") String enterEmail);
 }
