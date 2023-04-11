0e3d9b34fca8fe78229e32a9639d0d9f20510c4a
Ethorcharl <zenja_riga5@yahoo.com>
2023-04-11 17:57:27 +0300

    Update for registration

--- src/main/java/com/project/ServiceBooking/controllers/UserController.java
+++ src/main/java/com/project/ServiceBooking/controllers/UserController.java
@@ -30,23 +30,22 @@ public class UserController {
         this.emailService = emailService;
     }
 
-    @GetMapping("/registration")
-    public String showRegistrationForm(Model model) {
+    @GetMapping("/registration_client")
+    public String showRegistrationFormClient(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+    @GetMapping("/registration_specialist")
+    public String showRegistrationFormSpecialist(Model model) {
         model.addAttribute("user", new User());
         return "registration";
     }
     com.project.ServiceBooking.data.Role myRole = com.project.ServiceBooking.data.Role.CLIENT;
 
-    @PostMapping("/process_register")
-    public String addUser(User user, Model model) throws MessagingException {
+    @PostMapping("/process_register_client")
+    public String addClient(User user, Model model) throws MessagingException {
         User userFromDb = userService.findByEnterEmail(user.getEmail());
         if(userFromDb == null) {
-//            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//            String encodedPassword = passwordEncoder.encode(user.getPassword());
-//            user.setPassword(encodedPassword);
-//            user.setRole(Role.CLIENT);
-//            user.setStatus(Status.INACTIVE);
-//            userService.saveUser(user);
             userService.saveClient(user);
             //Verification
             // create token
@@ -61,6 +60,26 @@ public class UserController {
         }
         return "/login";
     }
+
+    @PostMapping("/process_register_specialist")
+    public String addSpecialist(User user, Model model) throws MessagingException {
+        User userFromDb = userService.findByEnterEmail(user.getEmail());
+        if(userFromDb == null) {
+            userService.saveSpecialist(user);
+            //Verification
+            // create token
+//            String token = UUID.randomUUID().toString();
+//            verificationTokenService.save(user, token);
+//            //send verification email
+//            emailService.sendHtmlMail(user);
+            model.addAttribute("message","A verification email has been sent ti your email address.");
+        }else{
+            //model.addAttribute("UserAlreadyExist", true);
+            model.addAttribute("message","User already exist.");
+        }
+        return "/login";
+    }
+
 
     @GetMapping("/activation")
     public String activation(@RequestParam("token") String token, Model model) throws MessagingException {
