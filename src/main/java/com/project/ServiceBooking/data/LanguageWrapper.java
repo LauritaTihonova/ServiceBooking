2272a388e76f04436474a04234f584bcad52f664
Kristiana Usacka <kristianausacka@gmail.com>
2023-04-14 11:54:07 +0300

    state before trying to implement language wrapper class method

---
+++ src/main/java/com/project/ServiceBooking/data/LanguageWrapper.java
@@ -0,0 +1,14 @@
+package com.project.ServiceBooking.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class LanguageWrapper {
+
+    private List<Language> languages;
+
+}
